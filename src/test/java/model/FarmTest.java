1a7e32504f438920e0eb66ea03fd92b1acbcbb17
Julio <julio.colon@emc.com>
2015-03-25 15:37:20 -0400

    Fixed Tests
    Change-Id: Id5760e86da9ef34401a206658221218417cc87d6

--- src/test/java/model/FarmTest.java
+++ src/test/java/model/FarmTest.java
@@ -1,14 +1,15 @@
+package model;
+
 import junit.framework.TestCase;
-import model.Bird;
 
 public class FarmTest extends TestCase {
     public FarmTest(String name) {
            super(name);
     }
 
-    public void BirdTest() {
+    public void testBird() {
         Bird bird = new Bird();
-        TestCase.assertEquals(bird, null);
+        TestCase.assertNotNull(bird);
     }
 }
 
